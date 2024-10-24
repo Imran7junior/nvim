44cf5f943bffb97146d88795340c92d85e8a8ad4
Md Imran Mansuri <md.imran.5682@gmail.com>
2024-10-25 00:35:59 +0530

    added plugins

--- README.md
+++
@@ -1,1 +0,0 @@
-# Personal neovim config and plugins
--- init.lua
+++
@@ -1,7 +0,0 @@
-require("im7ls.remap")
-require("im7ls.lazy_init")
-require("im7ls.userCommands")
-vim.cmd([[colorscheme tokyonight]])
-require("im7ls.lazy.lualine")
-
-
